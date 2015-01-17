93b1ac55ea7c6c8126e738c2168445c7e07afb32
Bobby Kostadinov <Bobby.Kostadinov@dowjones.com>
2015-01-17 14:26:21 +0000

    add nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,10 @@ var gulp = require('gulp'),
 git = require('gulp-git'),
 bump = require('gulp-bump'),
 filter = require('gulp-filter'),
-tag_version = require('gulp-tag-version');
+tag_version = require('gulp-tag-version'),
+gulpNodemon = require('gulp-nodemon');
 
 require('load-common-gulp-tasks')(gulp);
-
-function incTag(importance) {
-    return gulp.src(['./package.json'])
-    .pipe(bump({type: importance}))
-    .pipe(gulp.dest('./'))
-    .pipe(git.commit('bumps package version'))
-    .pipe(filter('package.json'))
-    .pipe(tag_version());
-}
 
 function inc(importance) {
     return gulp.src(['./package.json'])
@@ -23,9 +15,27 @@ function inc(importance) {
     .pipe(filter('package.json'))
     .pipe(tag_version());
 }
+gulp.task('start-watch', function () {
 
+    gulpNodemon({
+        script: 'server.js',
+        ext: 'js',
+    })
+    .on('change')
+    .on('restart', function () {
+        console.log('restarted!');
+    });
+});
 
-gulp.task('prerelease', 'makes v0.2.1 → v0.2.1-1', ['lint', 'felint', 'test', 'nice-package'], function() { return inc('prerelease'); });
-gulp.task('patch', 'makes v0.1.0 → v0.1.1', ['test', 'lint', 'nice-package', 'felint'], function() { return incTag('patch'); });
-gulp.task('feature', 'makes v0.1.1 → v0.2.0', ['lint', 'felint', 'test', 'nice-package'], function() { return incTag('minor'); });
-gulp.task('release', 'makes v0.2.1 → v1.0.0', ['lint', 'felint', 'test', 'nice-package'], function() { return incTag('major'); });
+function incNoTag(importance) {
+    return gulp.src(['./package.json'])
+    .pipe(bump({type: importance}))
+    .pipe(gulp.dest('./'))
+    .pipe(git.commit('bumps package version'))
+    .pipe(filter('package.json'));
+}
+
+gulp.task('prerelease', 'makes v0.2.1 → v0.2.1-1', ['lint', 'felint', 'test-cover', 'nice-package'], function() { return incNoTag('prerelease'); });
+gulp.task('patch', 'makes v0.1.0 → v0.1.1', ['test-cover', 'lint', 'nice-package', 'felint'], function() { return inc('patch'); });
+gulp.task('feature', 'makes v0.1.1 → v0.2.0', ['lint', 'felint', 'test-cover', 'nice-package'], function() { return inc('minor'); });
+gulp.task('release', 'makes v0.2.1 → v1.0.0', ['lint', 'felint', 'test-cover', 'nice-package'], function() { return inc('major'); });
