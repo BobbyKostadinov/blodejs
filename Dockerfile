5e0a2b24bc4ff0106eb9baf055cb79f370cd07c3
Bobby Kostadinov <Bobby.Kostadinov@dowjones.com>
2015-01-18 17:39:43 +0000

    docker

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM octohost/nodejs
+
+RUN add-apt-repository -y ppa:nginx/stable
+
+RUN apt-get update
+
+RUN apt-get -y install nginx make
+
+RUN mkdir /srv/www
+
+ADD default /etc/nginx/sites-available/default
+ADD nginx.conf /etc/nginx/nginx.conf
+
+WORKDIR /srv/www
+
+ADD . /srv/www/
+
+RUN export NODE_ENV=APP_ENV
+RUN npm install
+
+RUN apt-get update
+
+RUN export DEBIAN_FRONTEND=noninteractive
+RUN sudo apt-get -q -y install curl
+
+RUN npm install -g n
+RUN n 0.11.14
+
+RUN node --harmony /srv/www/server.js &
+
+EXPOSE 80
+
+CMD nginx
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-var http = require("http");
+var koa = require('koa');
+var app = koa();
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(process.env.PORT || 8888);	
+// x-response-time
+app.use(function *(next){
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    this.set('X-Response-Time', ms + 'ms');
+});
+
+// logger
+app.use(function *(next){
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+// response
+app.use(function *(){
+    this.body = 'Hello World';
+});
+
+app.listen(8888);
